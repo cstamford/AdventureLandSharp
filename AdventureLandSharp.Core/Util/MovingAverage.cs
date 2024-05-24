c9f39d4344381112cac4a94ea8990f81989cd086
cstamford <cstamford@gmail.com>
2024-05-24 01:41:56 +0100

    Minor changes to avoid exception when MovingAverage is empty.

--- AdventureLandSharp.Core/Util/MovingAverage.cs
+++ AdventureLandSharp.Core/Util/MovingAverage.cs
@@ -2,21 +2,28 @@ using System.Numerics;
 
 namespace AdventureLandSharp.Core.Util;
 
-public class MovingAverage<TValue>(TimeSpan window) where TValue: INumber<TValue> {
-    public TValue Min => _values.MinBy(x => x.Data).Data;
-    public TValue Sum => _values.Select(x => x.Data).Aggregate((acc, x) => acc + x);
-    public TValue Max => _values.MaxBy(x => x.Data).Data;
+public class MovingAverage<TValue>(TimeSpan window) where TValue: struct, INumber<TValue> {
+    public TValue Min => _values.Count == 0 ? default : _values.MinBy(x => x.Data).Data;
+    public TValue Sum => _values.Count == 0 ? default : _values.Select(x => x.Data).Aggregate((acc, x) => acc + x);
+    public TValue Max => _values.Count == 0 ? default : _values.MaxBy(x => x.Data).Data;
 
     public int Samples => _values.Count;
+    public TimeSpan Window => window;
 
-    public void Add(TValue value) {
-        DateTimeOffset now = DateTimeOffset.UtcNow;
+    public MovingAverage(MovingAverage<TValue> other) : this(other.Window) {
+        _values = new(other._values);
+    }
+
+    public void Add(TValue value) => Add(DateTimeOffset.UtcNow, value);
+
+    public void Add(DateTimeOffset now, TValue value) {
         _values.AddLast((now, value));
-
         while (_values.Count > 0 && now > _values.First!.Value.Time.Add(window)) {
             _values.RemoveFirst();
         }
     }
 
-     private readonly LinkedList<(DateTimeOffset Time, TValue Data)> _values = [];
+    public void Clear() => _values.Clear();
+
+    private readonly LinkedList<(DateTimeOffset Time, TValue Data)> _values = [];
 }
